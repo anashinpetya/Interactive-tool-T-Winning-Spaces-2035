c8e2eb6802d206406e17b598a8764d768d85a33e
anashinpetya <anashinpetya@gmail.com>
2025-11-21 12:43:28 +0200

    Update Car passengers comparison.py

--- pages/Car passengers comparison.py
+++ pages/Car passengers comparison.py
@@ -163,7 +163,7 @@ def kepler_config_lines(data_id, palette):
     return {
         "version": "v1",
         "config": {
-            "mapState": {"latitude": 60.26, "longitude": 24.9, "zoom": 8.73},
+            "mapState": {"latitude": 60.26, "longitude": 24.97, "zoom": 8.5},
             "mapStyle": {
                 "id": "carto_dark",
                 "label": "Carto Dark",
